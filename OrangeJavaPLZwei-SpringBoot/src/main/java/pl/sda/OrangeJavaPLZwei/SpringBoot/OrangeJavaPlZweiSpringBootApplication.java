4e00ab44008566b2c6c4d34df11c105950cfd2d2
Miko-Reach <127021089+Miko-Reach@users.noreply.github.com>
2023-04-18 20:19:19 +0200

    DODAJEMY SPRING BOOT
    Added newly generated Spring Boot

---
+++ OrangeJavaPLZwei-SpringBoot/src/main/java/pl/sda/OrangeJavaPLZwei/SpringBoot/OrangeJavaPlZweiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.OrangeJavaPLZwei.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeJavaPlZweiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeJavaPlZweiSpringBootApplication.class, args);
+	}
+
+}
---
+++ OrangeJavaPLZwei-SpringBoot/src/test/java/pl/sda/OrangeJavaPLZwei/SpringBoot/OrangeJavaPlZweiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.OrangeJavaPLZwei.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeJavaPlZweiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
